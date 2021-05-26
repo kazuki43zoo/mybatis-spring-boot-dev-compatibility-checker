9f3dddf19b75cf95d8d405c2aabd6a85842558b2
Kazuki Shimizu <kazuki43zoo@gmail.com>
2021-05-26 21:44:49 +0900

    Support Spring Boot 2.5

--- verify.sh
+++ verify.sh
@@ -19,7 +19,7 @@ END
   echo "${targetMinorVersion}.$(echo "${maintenanceVersions}" | sort -n | tail -n 1)${prefix}-SNAPSHOT"
 }
 
-TARGET_MINOR_VERSIONS="2.4 2.3 2.2 2.1"
+TARGET_MINOR_VERSIONS="2.5 2.4 2.3 2.2 2.1"
 
 for targetMinorVersion in ${TARGET_MINOR_VERSIONS}; do
   snapshotVersions="${snapshotVersions}$(getLatestMaintenanceVersion "${targetMinorVersion}")"$'\n'
